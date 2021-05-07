56e33d7de166ddaa0852f76bf35b17570b2e7805
mirnasalas <83840168+mirnasalas@users.noreply.github.com>
2021-05-07 16:35:25 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,71 @@
+basic.showString("#")
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(4000)
+basic.showLeds(`
+    # # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showString("#")
+basic.pause(1000)
+basic.forever(function () {
+	
+})
